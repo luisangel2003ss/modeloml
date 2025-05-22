1aedc7dd8f439bb2ba92fa9052abd568e033e44d
luis <luisangelss2003@gmail.com>
2025-05-21 21:50:22 -0500

    muevo

---
+++ versiones.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 21 15:57:43 2025
+
+@author: RAUL SANCHEZ
+"""
+
+import sklearn
+import tensorflow as tf
+import pandas as pd
+import streamlit as st
+import joblib
+
+print("scikit-learn version:", sklearn.__version__)
+print("tensorflow version:", tf.__version__)
+print("pandas version:", pd.__version__)
+print("streamlit version:", st.__version__)
+print("joblib version:", joblib.__version__)
