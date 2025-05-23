cf697dc331705ca53ed7a556650a13a9edde51f6
luis <luisangelss2003@gmail.com>
2025-05-22 20:18:09 -0500

    falto un req jahdjs

--- versiones.py
+++ versiones.py
@@ -11,6 +11,7 @@ import pandas as pd
 import streamlit as st
 import joblib
 import numpy 
+import matplotlib.pyplot
 
 print("scikit-learn version:", sklearn.__version__)
 print("tensorflow version:", tf.__version__)
@@ -18,3 +19,4 @@ print("pandas version:", pd.__version__)
 print("streamlit version:", st.__version__)
 print("numpy version:", numpy.__version__)
 print("joblib version:", joblib.__version__)
+print("matplot version:", matplotlib.__version__)
